f3803dbdaa98440a37b45235d22223c53691b123
FuYaoDe <dan826252005@yahoo.com.tw>
2014-04-29 10:23:28 +0800

    OK

--- src/com/example/navigationdrawer/Variable.java
+++ src/com/example/navigationdrawer/Variable.java
@@ -1,6 +1,7 @@
 package com.example.navigationdrawer;
 
 public final class Variable {
+	
 	public static final String SetName="Set";
 	public static final String[] SharedPreferencesText1= new String[]{"En","Math","Phy","Jock"};
 	public static final String[] SharedPreferencesText2= new String[]{"GoBed","GetUp","UsTran","Class"};
